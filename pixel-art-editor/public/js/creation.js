53296c49382819f8035d8b81809f22ba415bfa3f
Emmanuel <emanusdias@gmail.com>
2023-09-27 12:20:48 -0300

    upload seems to be working, always overwriting the same path

--- pixel-art-editor/public/js/creation.js
+++ pixel-art-editor/public/js/creation.js
@@ -4,10 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const boardSizeSelect = document.getElementById('board-size');
   const boardContainer = document.querySelector('.board-container');
   const clearBoardButton = document.getElementById('clear-board');
+  const uploadButton = document.getElementById('upload-button');
 
   // Função para criar o grid com base no tamanho selecionado
   function resizeBoard(size) {
-    const pixelSize = (boardContainer.offsetWidth - 2)/selectedSize;
+    const pixelSize = (boardContainer.offsetWidth - 2) / selectedSize;
     boardContainer.style.gridTemplateColumns = `repeat(${size}, ${pixelSize}px)`;
     boardContainer.style.gridTemplateRows = `repeat(${size}, ${pixelSize}px)`;
   }
@@ -15,30 +16,30 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('resize', () => {
     resizeBoard(selectedSize);
   });
-  
-  function createBoard(size) {
-    boardContainer.innerHTML = ''; 
-    
-    for (let i = 0; i < size * size; i++) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixel.addEventListener('mousedown', (e) => {
-          paintPixel(pixel, '#000')
-          e.preventDefault();
-        });
-        pixel.addEventListener('mouseover', (e) => {
-          if (e.buttons == 1) {
-            paintPixel(pixel, '#000');
-          }
-        });
-        boardContainer.appendChild(pixel);
-      }
 
-      resizeBoard(size);
+  function createBoard(size) {
+    boardContainer.innerHTML = '';
+
+    for (let i = 0; i < size * size; i++) {
+      const pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      pixel.addEventListener('mousedown', (e) => {
+        paintPixel(pixel, '#000')
+        e.preventDefault();
+      });
+      pixel.addEventListener('mouseover', (e) => {
+        if (e.buttons == 1) {
+          paintPixel(pixel, '#000');
+        }
+      });
+      boardContainer.appendChild(pixel);
+    }
+
+    resizeBoard(size);
   }
 
   function paintPixel(cell, color) {
-      cell.style.backgroundColor = color;
+    cell.style.backgroundColor = color;
   }
 
   // Inicializa o grid com o tamanho padrão (16x16)
@@ -46,15 +47,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Button event to change board size
   boardSizeSelect.addEventListener('change', function () {
-      selectedSize = parseInt(boardSizeSelect.value);
-      createBoard(selectedSize);
+    selectedSize = parseInt(boardSizeSelect.value);
+    createBoard(selectedSize);
   });
 
   // Button event to clear board
   clearBoardButton.addEventListener('click', function () {
-      const pixels = document.querySelectorAll('.pixel');
-      pixels.forEach(pixel => {
-        pixel.style.backgroundColor = '#fff';
-      });
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+      pixel.style.backgroundColor = '#fff';
+    });
+  });
+
+  // Button event to upload image
+  uploadButton.addEventListener('click', function () {
+    let binaryImage = [];
+    
+    boardContainer.childNodes.forEach((pixel) => {
+      if (pixel.style.backgroundColor !== '') {
+        binaryImage.push(1);
+      } else {
+        binaryImage.push(0);
+      }
+    })
+    // Send the image to the server using an AJAX request
+    fetch('/creation/generate-image', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ binaryImage }),
+    }).then(response => {
+      if (response.ok) {
+        // Parse the JSON response to access the imagePath
+        response.json().then(data => {
+          const imageResult = document.getElementById('upload-image');
+          imageResult.src = "../" + data.imagePath;
+        });
+      } else {
+        console.error('Error generating PNG image');
+      }
+    });
   });
 });
--- pixel-art-editor/routes/creation.js
+++ pixel-art-editor/routes/creation.js
@@ -2,9 +2,56 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+const fs = require('fs');
+const PNG = require('pngjs').PNG;
+
 // Route for displaying the creation page
 router.get('/', (req, res) => {
   res.render('creation');
 });
+
+router.post('/generate-image', (req, res) => {
+  const list = req.body.binaryImage;
+
+  // Calculate the width of the image based on the length of the list
+  const size = Math.sqrt(list.length);
+  if (size !== 16 && size !== 32 && size !== 64) {
+    return res.status(400).send('Invalid image size');
+  }
+
+  let png = new PNG({ 
+    width: size,
+    height: size,
+    colorType: 0,
+   });
+
+  // Loop through the list and set pixel colors
+  let idx = 0;
+  list.forEach(element => {
+    if (element === 1) {
+      png.data[4 * idx] = 0x00;
+      png.data[4 * idx + 1] = 0x00;
+      png.data[4 * idx + 2] = 0x00;
+      png.data[4 * idx + 3] = 0xFF;
+    } else if (element === 0) {
+      png.data[4 * idx] = 0xFF;
+      png.data[4 * idx + 1] = 0xFF;
+      png.data[4 * idx + 2] = 0xFF;
+      png.data[4 * idx + 3] = 0xFF;
+    } else {
+      return res.status(400).send('Invalid image input!');
+    }
+    idx += 1;
+  });
+
+  // Save the PNG image to a file (or you can use a buffer if you don't need to save it)
+  const imagePath = 'uploads/generated.png';
+  const imageStream = fs.createWriteStream(imagePath);
+  png.pack().pipe(imageStream);
+
+  // Send a JSON response with the status and image path
+  res.json({ status: 'OK', imagePath });
+});
+
 
 module.exports = router;
